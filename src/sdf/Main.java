43a3aa44a0f61ab0861fec2cdb9e4fc484d3e3b3
weizhen94 <weizhen94@gmail.com>
2023-01-27 12:26:03 +0800

    3rd commit

--- src/sdf/Main.java
+++ src/sdf/Main.java
@@ -1,7 +1,40 @@
-package sdf; 
+package sdf;
+
+import java.io.Console;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        
+        Console cons = System.console(); 
+        System.out.println("Welcome.");
+        String calculations = cons.readLine(">"); 
+        String calculatedString; 
+        double calculated; 
+
+        if(calculations.equalsIgnoreCase("Exit")){
+        System.out.println("Bye bye");
+        } 
+        // else {
+        //     String[] toCalculate = calculations.split(" "); 
+             
+        //     String valueOne = toCalculate[0]; 
+        //     double valueOneDouble = Double.parseDouble(valueOne);  
+
+        //     String valueThree = toCalculate[2]; 
+        //     double valueThreeDouble = Double.parseDouble(valueThree); 
+
+        //     String operation = toCalculate[1]; 
+
+        //     switch(operation){
+        //         case "+" -> { calculated = valueOneDouble + valueThreeDouble; }
+        //         case "-" -> { calculated = valueOneDouble - valueThreeDouble; }
+        //         case "*" -> { calculated = valueOneDouble * valueThreeDouble; }
+        //         case "/" -> { calculated = valueOneDouble / valueThreeDouble; }
+        //         default -> System.out.println("Please input a valid operation: '+ or - or * or / ");
+        //     }
+
+        //     calculatedString = Double.toString(calculated); 
+        //     System.out.println(calculatedString);
+        // }
     }
 }
